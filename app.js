62348191319ed341eb3f994d05d05ebb08f3c913
Priyanshu Nayan <priyanshunayan456@gmail.com>
2018-12-26 22:20:40 +0530

    first class functions

--- app.js
+++ app.js
@@ -3,3 +3,17 @@ const b = 2;
 const c = a+b;
 
 console.log(c);
+// Function Statement 
+
+function greet(){
+    console.log('Hi');
+}
+greet();
+
+
+//First class functions
+
+function logGreet(fn) {
+    fn();
+}
+logGreet(greet);
