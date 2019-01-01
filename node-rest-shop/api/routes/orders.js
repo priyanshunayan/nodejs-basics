b26f6160f114039f8a14741c7665848c88b634dd
Priyanshu Nayan <priyanshunayan456@gmail.com>
2019-01-01 13:16:42 +0530

    add routes to the api

--- node-rest-shop/api/routes/orders.js
+++ node-rest-shop/api/routes/orders.js
@@ -1,9 +1,22 @@
 const express = require('express');
 
-const app = express();
+const router = express.Router();
 
-app.get('/', (req, res, next)=>{
+router.get('/', (req, res, next)=>{
     res.status(200).json({
         message:"get request for orders"
     })
 })
+router.post('/', (req, res, next)=>{
+    res.status(201).json({
+        message:"postrequest for orders"
+    })
+})
+router.get('/:id', (req, res, next)=>{
+    res.status(201).json({
+        message:"get request for specific id",
+        id: req.params.id
+    })
+})
+
+module.exports = router;
--- node-rest-shop/app.js
+++ node-rest-shop/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
