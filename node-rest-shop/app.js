8f6f78f933a6e490bd3219312230efd0fabf06a3
Priyanshu Nayan <priyanshunayan456@gmail.com>
2019-01-02 13:34:09 +0530

    user login and signup api

--- node-rest-shop/app.js
+++ node-rest-shop/app.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
-
+const userRoutes = require('./api/routes/user');
 // Connecting to the Databases
 mongoose.connect('mongodb+srv://node-shop:' + process.env.MONGO_ATLAS_PS +'@cluster0-cwplo.mongodb.net/test?retryWrites=true', {useNewUrlParser: true});
 mongoose.Promise = global.Promise;
@@ -34,7 +34,7 @@ app.use((req, res, next) => {
 //Routes Handling
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes );
-
+app.use('/user', userRoutes);
 //Handling Errors
 
 //routes which don't match the above routes will pass through this and give error.
